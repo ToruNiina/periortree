1e85bdfb535ae68c4b81d1118f7aa9c593c9d6a8
ToruNiina <niina.toru.68u@gmail.com>
2017-09-01 08:21:21 +0900

    use boost/config

--- periortree/config.hpp
+++ periortree/config.hpp
@@ -1,5 +1,6 @@
 #ifndef PERIOR_TREE_CONFIG_HPP
 #define PERIOR_TREE_CONFIG_HPP
+#include <boost/config.hpp>
 
 #ifndef __cplusplus
 #error "this is c++ library but __cplusplus macro is not defined."
@@ -7,15 +8,7 @@
 
 #if __cplusplus >= 201103L
 #define PERIOR_TREE_ENABLE_CXX11 1
-#define PERIOR_TREE_CONSTEXPR    constexpr
-#define PERIOR_TREE_NOEXCEPT     noexcept
-#define PERIOR_TREE_OVERRIDE     override
-#define PERIOR_TREE_NULLPTR      nullptr
 #else
-#define PERIOR_TREE_CONSTEXPR
-#define PERIOR_TREE_NOEXCEPT  throw()
-#define PERIOR_TREE_OVERRIDE
-#define PERIOR_TREE_NULLPTR   NULL
 #endif
 
 #endif//PERIOR_TREE_CONFIG_HPP
