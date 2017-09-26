60533c7fe663c839b1777647364859021a840e2b
ToruNiina <niina.toru.68u@gmail.com>
2017-09-26 17:56:13 +0900

    fix expand: divide width by 2

--- periortree/expand.hpp
+++ periortree/expand.hpp
@@ -16,10 +16,10 @@ expand(const rectangle<pointT>& lhs, const rectangle<pointT>& rhs,
 {
     typedef typename traits::scalar_type_of<pointT>::type scalar_type;
 
-    const pointT l1 = lhs.centroid - lhs.width;
-    const pointT u1 = lhs.centroid + lhs.width;
-    const pointT l2 = rhs.centroid - rhs.width;
-    const pointT u2 = rhs.centroid + rhs.width;
+    const pointT l1 = lhs.centroid - lhs.width / 2;
+    const pointT u1 = lhs.centroid + lhs.width / 2;
+    const pointT l2 = rhs.centroid - rhs.width / 2;
+    const pointT u2 = rhs.centroid + rhs.width / 2;
 
     pointT center, width;
     for(std::size_t i=0; i<traits::dimension<pointT>::value; ++i)
@@ -43,10 +43,10 @@ expand(const rectangle<pointT>& lhs, const rectangle<pointT>& rhs,
     typedef typename traits::scalar_type_of<pointT>::type scalar_type;
 
     const pointT dc(restrict_direction(rhs.centroid - lhs.centroid, b));
-    const pointT l1 = lhs.centroid - lhs.width;
-    const pointT u1 = lhs.centroid + lhs.width;
-    const pointT l2 = lhs.centroid + dc - rhs.width;
-    const pointT u2 = lhs.centroid + dc + rhs.width;
+    const pointT l1 = lhs.centroid - lhs.width / 2;
+    const pointT u1 = lhs.centroid + lhs.width / 2;
+    const pointT l2 = lhs.centroid + dc - rhs.width / 2;
+    const pointT u2 = lhs.centroid + dc + rhs.width / 2;
 
     pointT center, width;
     for(std::size_t i=0; i<traits::dimension<pointT>::value; ++i)
@@ -67,8 +67,8 @@ expand(const rectangle<pointT>& rct, const pointT& p,
     BOOST_NOEXCEPT_OR_NOTHROW
 {
     typedef typename traits::scalar_type_of<pointT>::type scalar_type;
-    const pointT lower = rct.centroid - rct.width;
-    const pointT upper = rct.centroid + rct.width;
+    const pointT lower = rct.centroid - rct.width / 2;
+    const pointT upper = rct.centroid + rct.width / 2;
 
     pointT center, width;
     for(std::size_t i=0; i<traits::dimension<pointT>::value; ++i)
@@ -91,8 +91,8 @@ expand(const rectangle<pointT>& rct, const pointT& p,
     typedef typename traits::scalar_type_of<pointT>::type scalar_type;
 
     const pointT dc(restrict_direction(p - rct.centroid, b));
-    const pointT lower = rct.centroid - rct.width;
-    const pointT upper = rct.centroid + rct.width;
+    const pointT lower = rct.centroid - rct.width / 2;
+    const pointT upper = rct.centroid + rct.width / 2;
     const pointT p_ = rct.centroid + dc;
 
     pointT center, width;
